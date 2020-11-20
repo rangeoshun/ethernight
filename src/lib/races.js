4391fc1a7e05433a2c9aef1caff070af38c2f810
Abel Varga <avarga@hajtospincer.hu>
2020-11-20 09:17:16 +0100

    Add basic races abilities

---
+++ src/lib/races.js
@@ -0,0 +1,52 @@
+const RACES = {
+  hillDwarf: {
+    abilities: {
+      constitution: 2,
+      wisdom: 1,
+    },
+  },
+  mountainDwarf: {
+    abilities: {
+      strength: 2,
+      constitution: 2,
+    },
+  },
+  highElf: {
+    abilities: {
+      dexterity: 2,
+      intelligence: 1,
+    },
+  },
+  woodElf: {
+    abilities: {
+      dexterity: 2,
+      widsom: 1,
+    },
+  },
+  lightfootHalfling: {
+    abilities: {
+      dexterity: 2,
+      charisma: 1,
+    },
+  },
+  stoutHalfling: {
+    abilities: {
+      constitution: 1,
+      dexterity: 2,
+    },
+  },
+  human: {
+    abilities: {
+      strength: 1,
+      dexterity: 1,
+      constitution: 1,
+      intelligence: 1,
+      wisdom: 1,
+      charisma: 1,
+    },
+  },
+};
+
+module.exports = {
+  RACES,
+};

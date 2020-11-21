24d257ff46be6bb84b91b1cfaa6ea553bc2d325d
Abel Varga <avarga@hajtospincer.hu>
2020-11-21 18:46:18 +0100

    Refactor race shape

--- src/lib/races.js
+++ src/lib/races.js
@@ -1,60 +1,56 @@
+const { ABILITIES } = require("./abilities");
+
 const RACES = {
   dwarf: {
-    abilities: {
-      constitution: 2,
-      wisdom: 1,
-    },
+    constitution: 2,
+    wisdom: 1,
   },
   elf: {
-    abilities: {
-      dexterity: 2,
-      intelligence: 1,
-    },
+    dexterity: 2,
+    intelligence: 1,
   },
   halfling: {
-    abilities: {
-      dexterity: 2,
-      charisma: 1,
-    },
+    dexterity: 2,
+    charisma: 1,
   },
   human: {
-    abilities: {
-      strength: 1,
-      dexterity: 1,
-      constitution: 1,
-      intelligence: 1,
-      wisdom: 1,
-      charisma: 1,
-    },
+    strength: 1,
+    dexterity: 1,
+    constitution: 1,
+    intelligence: 1,
+    wisdom: 1,
+    charisma: 1,
   },
   dragonborn: {
-    abilities: {
-      strength: 2,
-      charisma: 1,
-    },
+    strength: 2,
+    charisma: 1,
   },
   gnome: {
-    abilities: {
-      intelligence: 2,
-    },
+    intelligence: 2,
   },
   halfElf: {
-    abilities: {
-      charisma: 2,
-      custom: [1, 1],
-    },
+    charisma: 2,
+    custom: [1, 1],
   },
   halfOrk: {
-    abilities: {
-      strength: 2,
-      constitution: 1,
-    },
+    strength: 2,
+    constitution: 1,
   },
   tiefling: {
     intelligence: 1,
     charisma: 2,
   },
 };
+
+// TODO: Find out if this is useful at all or not.
+const applyRacialModifiers = (abilities, race) =>
+  abilities.reduce(
+    (acc, ability, modifier_) => (
+      (modifier_ = race[ability]),
+      (!modifier_ && acc) || { ...acc, [ability]: acc[ability] + modifier_ }
+    ),
+    abilities
+  );
 
 module.exports = {
   RACES,

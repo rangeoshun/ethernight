21a1fbc067a9f016138e7b820de6cf44249db18d
Abel Varga <avarga@hajtospincer.hu>
2020-11-21 22:56:28 +0100

    Add unarmedStrike and martialArts

--- src/lib/classes.js
+++ src/lib/classes.js
@@ -1,5 +1,5 @@
 const { uniq } = require("lodash/fp");
-const { die } = require("./dice");
+const { die, roll } = require("./dice");
 
 const CLASSES = {
   barbarian: {
@@ -7,72 +7,88 @@ const CLASSES = {
     primary: ["strength"],
     saving: ["strength", "constitution"],
     unarmoredDefense: [10, "dexterity", "constitution"],
+    unarmedSrike: [1, "strength"],
   },
   bard: {
     hitDie: 8,
     primary: ["charisma"],
     saving: ["dexterity", "charisma"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   cleric: {
     hitDie: 8,
     primary: ["wisdom"],
     saving: ["wisdom", "charisma"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   druid: {
     hitDie: 8,
     primary: "wisdom",
     saving: ["intelligence", "wisdom"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   fighter: {
     hitDie: 10,
     primary: ["strength"],
     saving: ["strength", "constitution"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   monk: {
     hitDie: 8,
     primary: ["strength", "wisdom"],
     saving: ["strength", "dexterity"],
     unarmoredDefense: [10, "dexterity", "wisdom"],
+    unarmedSrike: [1, "strength"],
+    martialArts: [
+      die(4),
+      (monk) => (monk.strength > monk.dexterity && "strength") || "dexterity",
+    ],
   },
   paladin: {
     hitDie: 10,
     primary: ["strength", "charisma"],
     saving: ["wisdom", "charisma"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   ranger: {
     hitDie: 10,
     primary: ["dexterity", "wisdom"],
     saving: ["strength", "dexterity"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   rouge: {
     hitDie: 8,
     primary: ["dexterity"],
     saving: ["dexterity", "intelligence"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   sorcerer: {
     hitDie: 6,
     primary: ["charisma"],
     saving: ["constitution", "charisma"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   warlock: {
     hitDie: 8,
     primary: ["charisma"],
     saving: ["wisdom", "charisma"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
   wizard: {
     hitDie: 6,
     primary: ["intelligence"],
     saving: ["intelligence", "wisdom"],
     unarmoredDefense: [10, "dexterity"],
+    unarmedSrike: [1, "strength"],
   },
 };
 
@@ -83,7 +99,7 @@ const unarmoredDefense = (modifiers, components) =>
 
 const hitPoints = (hitDie, modifier, previous = 0, useDice = false, value_) => (
   (value_ =
-    (useDice && staticHitDie(hitDie)) || (previous && die(hitDie)) || hitDie),
+    (useDice && staticHitDie(hitDie)) || (previous && roll(hitDie)) || hitDie),
   previous + value_ + modifier
 );
 

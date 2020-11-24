1c230ddba7fd2f78bf7e76f54687746d24eda39d
Abel Varga <avarga@hajtospincer.hu>
2020-11-24 23:53:27 +0100

    Totally refactor combat

--- src/lib/__tests__/combat.test.js
+++ src/lib/__tests__/combat.test.js
@@ -1,22 +1,27 @@
 const ch = require("../character");
 const c = require("../combat");
 
-describe(c.attack, () => {
-  [
-    ["critical", 1, 2, 20],
-    ["hit", 1, 3, 2],
-    ["miss", 1, 21, 19],
-    ["failure", 1, 0, 1],
-  ].forEach(([expected, ...args]) =>
-    it(`${expected} when modifier is ${args[0]} against AC ${args[1]} with roll ${args[2]}`, () =>
-      expect(c.attack(...args)).toEqual(expected))
-  );
-});
+describe(c.attack, () => {});
 
 describe(c.combat, () => {
-  ben = ch.generate("halfling", "monk");
+  const ben = ch.generate("halfling", "monk");
   ben.name = "ben the monk";
-  bob = ch.generate("halfOrk", "wizard");
+  const bob = ch.generate("halfOrk", "wizard");
   bob.name = "bob the wizard";
-  it("ben vs bob", () => expect(c.fight([bob, ben])).toBeTruthy());
+  const action = {
+    inflictor: ben,
+    kind: "attack",
+    weapon: "unarmedStrike",
+    modifier: "strength",
+    target: bob,
+  };
+
+  it("ben vs bob", () =>
+    expect(c.turn([action])).toEqual([
+      expect.objectContaining({
+        inflictor: expect.objectContaining(ben),
+        target: expect.objectContaining(bob),
+        outcome: expect.stringMatching(/critical|hit|miss|failure/),
+      }),
+    ]));
 });
--- src/lib/combat.js
+++ src/lib/combat.js
@@ -1,3 +1,4 @@
+const { includes } = require("lodash/fp");
 const c = require("./character");
 const cl = require("./classes");
 const d = require("./dice");
@@ -10,7 +11,7 @@ const initiative = (characters) =>
     }))
     .sort((a, b) => a.lastDexterityCheck - b.lastDexterityCheck);
 
-const attack = (modifier, defenderArmorClass, roll_) => (
+const hit = (modifier, defenderArmorClass, roll_) => (
   (roll_ = roll_ || d.d20()),
   (roll_ === 1 && "failure") ||
     (roll_ === 20 && "critical") ||
@@ -18,45 +19,31 @@ const attack = (modifier, defenderArmorClass, roll_) => (
     "miss"
 );
 
-const fight = (characters = [], ordered_ = false, ended_ = false) => (
-  ordered_ || fight(initiative(characters), true),
-  (ordered_ && ended_ && characters) ||
-    (!ended_ &&
-      ((characters = characters.reduce(
-        ([first, ...rest], next_, others_, attack_, damage_) => (
-          ([next_, ...others_] = rest),
-          console.log(`${first.name} attacks ${next_.name}`),
-          (attack_ = attack(first.strength, next_.armorClass)),
-          ((damage_ = cl.calculateChain(first, "unarmedStrike")),
-          console.log(`it's a ${attack_}`),
-          attack_ === "critical" || "hit",
-          (console.log(
-            `${first.name} injures ${next_.name} (${
-              next_.hitPoints - (next_.damage || 0)
-            }/${next_.hitPoints} HP) (-${damage_} HP)`
-          ),
-          (next_ = {
-            ...next_,
-            damage: (next_.damage || 0) + damage_,
-          }))) ||
-            (attack_ === "failure" &&
-              (consol.log(`${first.name} injures self (-${damage_})`),
-              (first = {
-                ...first,
-                damage: (first.damage || 0) + damage_,
-              }))),
-          [next_, ...others_, first]
-        ),
-        characters
-      )),
-      (ended_ = characters.reduce(
-        (acc, curr) =>
-          acc ||
-          (curr.damage >= curr.hitPoints &&
-            !console.log(`${curr.name} is knocked out`)),
-        false
-      )),
-      fight(characters, true, ended_)))
+const damageModifiers = {
+  critical: 2,
+  hit: 1,
+  miss: 0,
+  failure: -1,
+};
+
+const damage = (inflictor, weapon, outcome) =>
+  cl.calculateChain(inflictor, weapon) * damageModifiers[outcome];
+
+const attack = ({ inflictor, target, weapon, modifier }, damage_, outcome_) => (
+  (outcome_ = hit(inflictor.modifiers[modifier], target.armorClass)),
+  (damage_ = damage(inflictor, weapon, outcome_)),
+  { inflictor, target, kind: "damage", damage: damage_, outcome: outcome_ }
 );
 
-module.exports = { attack, fight };
+const actions = {
+  attack,
+};
+
+const actionList = Object.keys(actions);
+
+const turn = ([first, ...rest], output_ = []) =>
+  (!first && output_) ||
+  (includes(first.kind, actionList) &&
+    turn(rest, [...output_, actions[first.kind](first)]));
+
+module.exports = { attack, initiative, turn };
